7b7a03b920d137d4cfa26b91bebd106e44a184bf
David Zollikofer <david@zollikofer.co>
2018-11-16 09:39:37 +0100

    added files

---
+++ sorting/SearchAlgorithmsTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SearchAlgorithmsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
