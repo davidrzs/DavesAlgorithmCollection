d68ff30e7aa1168ca052d3ba1fbee4619adbcdd8
David Zollikofer <david@zollikofer.co>
2018-11-19 19:29:29 +0100

    added list based graph

--- graphs/AdjacencyListGraph.java
+++ graphs/AdjacencyListGraph.java
@@ -1,4 +1,41 @@
+import java.util.List;
 
-public class AdjacencyListGraph {
+public class AdjacencyListGraph implements Graph {
+	
+	private int E;
+	private final int V;
+	
+	// we need to do this since I have a second linked list in my buildpath.
+	private java.util.LinkedList<Integer>[] nodes;
+	
+	// to get rid of the array of lists flaw in java: https://stackoverflow.com/questions/217065/cannot-create-an-array-of-linkedlists-in-java
+	@SuppressWarnings("unchecked")
+	public AdjacencyListGraph(int vertices){
+		this.V = vertices;
+		nodes = new java.util.LinkedList[vertices];
+		// now we initialize all of them
+		for(int i = 0; i < V; i++) {
+			nodes[i] = new java.util.LinkedList<Integer>();
+		}
+	}
+	
+	public int V() {
+		return V;
+	}
+	
+	public int E() {
+		return E;
+	}
+	
+	public void addEdge(int v, int w) {
+		nodes[v].add(w);
+		nodes[w].add(v);
+	}
+	
+	
+	public List<Integer> adj(int w){
+		return nodes[w];
+	}
+
 
 }
---
+++ graphs/Graph.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public interface Graph {
+	
+	
+	// inspired by Sedgewick/Wayne Algorithms 4
+	
+	int V();
+	
+	int E();
+	
+	void addEdge(int v, int w);
+		
+	List<Integer> adj();
+
+}
