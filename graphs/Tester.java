ab7ffe7142bee1aaafe1a3a4619781f06e54e3a5
David Zollikofer <david@zollikofer.co>
2018-12-15 01:11:27 +0100

    added weighted matrix graph datastructure & jgraph

---
+++ graphs/Tester.java
@@ -0,0 +1,10 @@
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.io.*;
+import org.jgrapht.traverse.*;
+
+public class Tester {
+public static void main(String[] args) {
+	System.out.println(12);
+}
+}
---
+++ graphs/WeightedMatrixGraph.java
@@ -0,0 +1,51 @@
+import java.util.List;
+
+public class WeightedMatrixGraph {
+
+
+		
+	private int E;
+	private final int V;
+	
+	// we need to do this since I have a second linked list in my buildpath.
+	private int[][] matrix;
+
+
+	public WeightedMatrixGraph(int vertices){
+		this.V = vertices;
+		matrix = new int[V][V];
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix.length; j++) {
+				matrix[i][j] = Integer.MIN_VALUE;
+			}			
+		}
+	}
+	
+	public int V() {
+		return V;
+	}
+	
+	public int E() {
+		return E;
+	}
+	
+	public void addEdge(int v, int w, int weight) {
+		matrix[v][w] = weight;
+	}
+	
+	
+	public List<Integer> adj(int w){
+		List<Integer> li = new java.util.LinkedList<Integer>();
+		for(int k = 0; k < V; k++) {
+			if(matrix[w][k] != Integer.MIN_VALUE) {
+				li.add(k);
+			}
+		}
+		return li;
+	}
+
+
+
+}
+
+
